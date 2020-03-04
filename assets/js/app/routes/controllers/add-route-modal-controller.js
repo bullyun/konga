ab2759ce5275dddecb63a4261effd3053a36f4ff
manniu <bullyun@aliyun.com>
2020-03-04 17:23:01 +0800

    feat: 新建 routes 时，支持 headers

--- assets/js/app/routes/controllers/add-route-modal-controller.js
+++ assets/js/app/routes/controllers/add-route-modal-controller.js
@@ -3,90 +3,119 @@
  *
  * Note that this file should only contain controllers and nothing else.
  */
-(function () {
+(function() {
   'use strict';
 
-  angular.module('frontend.routes')
-    .controller('AddRouteModalController', [
-      '_','$scope', '$rootScope', '$log', '$state', 'RoutesService', 'SettingsService',
-      '$uibModalInstance', 'MessageService', '_service',
-      function controller(_, $scope, $rootScope, $log, $state, RoutesService, SettingsService,
-                          $uibModalInstance, MessageService, _service) {
+  angular.module('frontend.routes').controller('AddRouteModalController', [
+    '_',
+    '$scope',
+    '$rootScope',
+    '$log',
+    '$state',
+    'RoutesService',
+    'SettingsService',
+    '$uibModalInstance',
+    'MessageService',
+    '_service',
+    function controller(
+      _,
+      $scope,
+      $rootScope,
+      $log,
+      $state,
+      RoutesService,
+      SettingsService,
+      $uibModalInstance,
+      MessageService,
+      _service
+    ) {
+      var availableFormattedVersion = RoutesService.getLastAvailableFormattedVersion(
+        $rootScope.Gateway.version
+      );
+      $scope.service = _service;
+      $scope.route = angular.copy(
+        RoutesService.getProperties($rootScope.Gateway.version)
+      );
+      // Assign service id
+      $scope.route.service = {
+        id: _service.id
+      };
 
+      $scope.partial =
+        'js/app/routes/partials/form-route-' +
+        availableFormattedVersion +
+        '.html?r=' +
+        Date.now();
 
-        var availableFormattedVersion = RoutesService.getLastAvailableFormattedVersion($rootScope.Gateway.version);
-        $scope.service = _service;
-        $scope.route = angular.copy(RoutesService.getProperties($rootScope.Gateway.version));
-        // Assign service id
-        $scope.route.service = {
-          id: _service.id
-        };
+      console.log('$scope.route', $scope.route, _service.id);
 
-        $scope.partial = 'js/app/routes/partials/form-route-' + availableFormattedVersion + '.html?r=' + Date.now();
+      $scope.close = function() {
+        $uibModalInstance.dismiss();
+      };
 
-        console.log("$scope.route", $scope.route, _service.id)
-
-        $scope.close = function () {
-          $uibModalInstance.dismiss()
+      $scope.onTagInputKeyPress = function($event) {
+        if ($event.keyCode === 13) {
+          if (!$scope.route.tags) $scope.route.tags = [];
+          $scope.route.tags = $scope.route.tags.concat(
+            $event.currentTarget.value
+          );
+          $event.currentTarget.value = null;
         }
+      };
 
-        $scope.onTagInputKeyPress = function ($event) {
-          if($event.keyCode === 13) {
-            if(!$scope.route.tags) $scope.route.tags = [];
-            $scope.route.tags = $scope.route.tags.concat($event.currentTarget.value);
-            $event.currentTarget.value = null;
-          }
-        }
+      $scope.submit = function() {
+        clearRoute();
 
+        const route = $scope.route;
 
-        $scope.submit = function () {
+        $scope.errorMessage = '';
 
-          clearRoute()
-
-          console.log("Route =>", $scope.route);
-          $scope.errorMessage = '';
-
-          RoutesService.add($scope.route)
-            .then(function (res) {
-              $rootScope.$broadcast('route.created')
-              MessageService.success('Route created!')
-              $uibModalInstance.dismiss(res);
-            }).catch(function (err) {
-            $log.error("Create new route error:", err)
-            MessageService.error("Submission failed. " + _.get(err,'data.body.message',""));
-            $scope.errors = {}
+        new Promise(resolve => {
+          resolve(
+            RoutesService.add(
+              Object.assign(route, {
+                headers: JSON.parse(route.headers.replace(/'/g, '"'))
+              })
+            )
+          );
+        })
+          .then(function(res) {
+            $rootScope.$broadcast('route.created');
+            MessageService.success('Route created!');
+            $uibModalInstance.dismiss(res);
+          })
+          .catch(function(err) {
+            $log.error('Create new route error:', err);
+            MessageService.error(
+              'Submission failed. ' +
+                _.get(err, 'data.body.message', err.message || err)
+            );
+            $scope.errors = {};
             const errorBody = _.get(err, 'data.body');
             if (errorBody) {
               if (errorBody.fields) {
-
                 for (let key in errorBody.fields) {
-                  $scope.errors[key] = errorBody.fields[key]
+                  $scope.errors[key] = errorBody.fields[key];
                 }
               }
               $scope.errorMessage = errorBody.message || '';
             } else {
-              $scope.errorMessage = "An unknown error has occured"
+              $scope.errorMessage = 'An unknown error has occured';
             }
+          });
+      };
 
-          })
-        }
+      function clearRoute() {
+        for (var key in $scope.route) {
+          if ($scope.route[key] instanceof Array && !$scope.route[key].length) {
+            delete $scope.route[key];
+          }
 
-
-        function clearRoute() {
-          for (var key in $scope.route) {
-
-            if ($scope.route[key] instanceof Array && !$scope.route[key].length) {
-              delete($scope.route[key]);
-            }
-
-            if ($scope.route[key] === undefined || $scope.route[key] === "") {
-              delete($scope.route[key]);
-            }
+          if ($scope.route[key] === undefined || $scope.route[key] === '') {
+            delete $scope.route[key];
           }
         }
-
-
       }
-    ])
-  ;
-}());
+    }
+  ]);
+})();
--- assets/js/app/routes/routes-service.js
+++ assets/js/app/routes/routes-service.js
@@ -3,109 +3,120 @@
  *
  * Note that this file should only contain controllers and nothing else.
  */
-(function () {
+(function() {
   'use strict';
 
-  angular.module('frontend.routes')
-    .service('RoutesService', [
-      '$log', '$state', '$http', 'Semver',
-      function ($log, $state, $http, Semver) {
-
-
-        /**
-         *
-         * IMPORTANT!!
-         * Each key must have a respective .html file in /routes/partials
-         */
-        var properties = {
-          '013': {
-            name: '',
-            hosts: [],
-            protocols: [],
-            methods: [],
-            paths: [],
-            strip_path: true,
-            preserve_host: false,
-            https_redirect_status_code: 426,
-            regex_priority: 0
-          }
+  angular.module('frontend.routes').service('RoutesService', [
+    '$log',
+    '$state',
+    '$http',
+    'Semver',
+    function($log, $state, $http, Semver) {
+      /**
+       *
+       * IMPORTANT!!
+       * Each key must have a respective .html file in /routes/partials
+       */
+      var properties = {
+        '013': {
+          name: '',
+          hosts: [],
+          protocols: [],
+          methods: [],
+          paths: [],
+          headers: '',
+          strip_path: true,
+          preserve_host: false,
+          https_redirect_status_code: 426,
+          regex_priority: 0
         }
+      };
 
-        return {
+      return {
+        getProperties: function(version) {
+          var fver = version
+            .split('.')
+            .slice(0, -1)
+            .join('');
+          var props =
+            properties[fver] ||
+            properties[
+              Object.keys(properties)[Object.keys(properties).length - 1]
+            ];
 
-          getProperties: function (version) {
+          return props;
+        },
 
-            var fver = version.split('.').slice(0, -1).join('');
-            var props = properties[fver] || properties[Object.keys(properties)[Object.keys(properties).length - 1]];
+        getLastAvailableFormattedVersion: function(version) {
+          var fver = version
+            .split('.')
+            .slice(0, -1)
+            .join('');
 
-            return props;
-          },
+          var existing =
+            Object.keys(properties).indexOf(fver) >= 0
+              ? fver
+              : Object.keys(properties)[Object.keys(properties).length - 1];
 
-          getLastAvailableFormattedVersion: function (version) {
+          return existing;
+        },
 
-            var fver = version.split('.').slice(0, -1).join('');
+        all: function() {
+          return $http.get('kong/routes');
+        },
 
-            var existing = Object.keys(properties).indexOf(fver) >= 0 ? fver : Object.keys(properties)[Object.keys(properties).length - 1];
+        findById: function(routeId) {
+          return $http.get('kong/routes/' + routeId);
+        },
 
-            return existing;
-          },
+        update: function(routeId, data) {
+          return $http.patch('kong/routes/' + routeId, data);
+        },
 
-          all: function () {
-            return $http.get('kong/routes')
-          },
+        delete: function(route) {
+          return $http.delete('kong/routes/' + route.id);
+        },
 
-          findById: function (routeId) {
-            return $http.get('kong/routes/' + routeId)
-          },
+        add: function(route) {
+          return $http.post('kong/routes/', route);
+        },
 
-          update: function (routeId, data) {
-            return $http.patch('kong/routes/' + routeId, data)
-          },
+        plugins: function(routeId) {
+          return $http.get('kong/routes/' + routeId + '/plugins');
+        },
 
-          delete: function (route) {
-            return $http.delete('kong/routes/' + route.id);
-          },
-
-          add: function (route) {
-            return $http.post('kong/routes/', route);
-          },
-
-          plugins: function (routeId) {
-            return $http.get('kong/routes/' + routeId + '/plugins')
-          },
-
-
-          addPlugin: function (routeId, plugin) {
-            for (var key in plugin) {
-              if (!plugin[key] || plugin[key] == '') delete plugin[key]
-            }
-
-            return $http.post('kong/routes/' + routeId + '/plugins', plugin)
-
-
-          },
-
-          consumers: function (routeId) {
-            return $http.get('api/kong_routes/' + routeId + '/consumers')
-          },
-
-          updatePlugin: function (routeId, pluginId, data) {
-
-            if (data.config) {
-              for (var key in data.config) {
-                data['config.' + key] = data.config[key]
-              }
-              delete data.config
-            }
-
-            return $http.patch('kong/routes/' + routeId + '/plugins/' + pluginId, data)
-          },
-
-          deletePlugin: function (routeId, pluginId) {
-            return $http.delete('kong/routes/' + routeId + '/plugins/' + pluginId)
+        addPlugin: function(routeId, plugin) {
+          for (var key in plugin) {
+            if (!plugin[key] || plugin[key] == '') delete plugin[key];
           }
+
+          return $http.post('kong/routes/' + routeId + '/plugins', plugin);
+        },
+
+        consumers: function(routeId) {
+          return $http.get('api/kong_routes/' + routeId + '/consumers');
+        },
+
+        updatePlugin: function(routeId, pluginId, data) {
+          if (data.config) {
+            for (var key in data.config) {
+              data['config.' + key] = data.config[key];
+            }
+            delete data.config;
+          }
+
+          return $http.patch(
+            'kong/routes/' + routeId + '/plugins/' + pluginId,
+            data
+          );
+        },
+
+        deletePlugin: function(routeId, pluginId) {
+          return $http.delete(
+            'kong/routes/' + routeId + '/plugins/' + pluginId
+          );
         }
-      }
-    ])
-  ;
-}());
+      };
+    }
+  ]);
+})();
